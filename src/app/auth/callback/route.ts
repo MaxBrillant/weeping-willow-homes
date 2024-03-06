f37eb26ceca7e255404b41e2d0c4a560b6baa02f
Max <ndashimax37@gmail.com>
2024-03-07 01:32:05 +0300

    I hope this is the final fix

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -4,7 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
-  console.warn("Initial url: " + requestUrl);
   const code = requestUrl.searchParams.get("code");
 
   if (code) {
@@ -12,8 +11,6 @@ export async function GET(request: NextRequest) {
     const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
     await supabase.auth.exchangeCodeForSession(code);
   }
-
-  console.warn("End url: " + requestUrl);
 
   // URL to redirect to after sign in process completes
   return NextResponse.redirect(requestUrl.origin);
