6b418ff031cfb4a010f9df3163a0836693a9058e
Max <ndashimax37@gmail.com>
2024-03-06 22:21:12 +0300

    Trying again

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,9 +1,8 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  console.log(request);
+export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
 
@@ -11,5 +10,5 @@ export async function GET(request: Request) {
     const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
   }
-  return NextResponse.redirect(requestUrl.origin);
+  return NextResponse.redirect(request.url);
 }
